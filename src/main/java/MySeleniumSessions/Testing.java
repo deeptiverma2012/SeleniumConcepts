be92dfd821fce76556d61a7acfd5ed98b4cf3bc0
deeptiverma2012 <2012deeptiv@gmail.com>
2023-12-18 11:39:38 -0600

    Tracking changes in 2 new files

--- src/main/java/MySeleniumSessions/Testing.java
+++ src/main/java/MySeleniumSessions/Testing.java
@@ -5,4 +5,9 @@ public class Testing {
 	public void test1() {
 		System.out.println("Test 1");
 	}
+	
+	public void test2()
+	{
+		System.out.println("Test 2");
+	}
 }
---
+++ src/main/java/MySeleniumSessions/Testing1.java
@@ -0,0 +1,9 @@
+package MySeleniumSessions;
+
+public class Testing1 {
+
+	public void test1()
+	{
+		System.out.println("New test1");
+	}
+}
